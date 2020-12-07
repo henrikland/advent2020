0d2315015bc7ec80bef87a7758a86c08ee2e2ca8
Henrik Persson <henrik@ustwo.com>
2020-12-07 11:32:18 +0100

    Day 7

---
+++ 7-1.py
@@ -0,0 +1,26 @@
+import sys
+import re
+
+def parseRule(rule):
+  cleaned_rule = re.sub(r"( \d )|\sbags?\s?\.?|no other bags\.", "", rule)
+  [node, children] = cleaned_rule.split("contain")
+  return (node, None if len(children) == 1 else children.split(","))
+
+nodes = {}
+
+for rule in sys.stdin.read().split("\n"):
+  (node, children) = parseRule(rule)
+  nodes[node] = children
+
+def findShinyGold(node):
+  if node == "shiny gold":
+    return 1
+
+  children = nodes[node]
+
+  if children == None:
+    return 0
+
+  return any(findShinyGold(child) for child in children)
+
+print(sum([findShinyGold(key) for key in nodes]) - 1) # -1 because we don't count the shiny gold bag
---
+++ 7-2.py
@@ -0,0 +1,38 @@
+import sys
+import re
+
+child_group_pattern = re.compile(r"(\d+) ([\w\s]+)")
+
+def parseRule(rule):
+  cleaned_rule = re.sub(r"\sbags?\s?\.?|no other bags\.", "", rule)
+  [node, contents] = cleaned_rule.split("contain ")
+  if len(contents) < 2:
+    return (node, None)
+
+  children = []
+
+  for child in contents.split(", "):
+    match = child_group_pattern.match(child)
+    children.append((int(match.group(1)), match.group(2)))
+
+  return (node, children)
+
+nodes = {}
+
+for rule in sys.stdin.read().split("\n"):
+  (node, children) = parseRule(rule)
+  nodes[node] = children
+
+def countBags(key):
+  children = nodes[key]
+  if children is None:
+    return 1
+
+  count = 1
+
+  for child in children:
+    count += child[0] * countBags(child[1])
+
+  return count
+
+print(countBags("shiny gold") - 1) # -1 because we don't count the shiny gold bag
