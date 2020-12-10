a8a54f184c08e9b9068e02a0dd8f534e9b5d1f97
Henrik Persson <henrik@ustwo.com>
2020-12-10 13:28:35 +0100

    Cleaning

--- 10-2.py
+++ 10-2.py
@@ -8,15 +8,15 @@ adapters = [
     input[-1] + 3
 ]
 
-cache_paths = [None for i in range(len(adapters))]
+cache_paths = [None for _ in range(len(adapters))]
 
 
-def pathsForIndex(path_index):
-    if cache_paths[path_index] is not None:
+def pathsForIndex(adapter_index):
+    if cache_paths[adapter_index] is not None:
         # We have already checked this item
-        return cache_paths[path_index]
+        return cache_paths[adapter_index]
 
-    if path_index == len(adapters) - 2:
+    if adapter_index == len(adapters) - 2:
         # We are at the second last item, there is only 1 possible path to the end
         return 1
 
@@ -24,13 +24,13 @@ def pathsForIndex(path_index):
         map(
             lambda a: pathsForIndex(a),
             filter(
-                lambda a: adapters[a] - adapters[path_index] < 4,
-                range(path_index + 1, min(path_index + 4, len(adapters)))
+                lambda a: adapters[a] - adapters[adapter_index] < 4,
+                range(adapter_index + 1, min(adapter_index + 4, len(adapters)))
             )
         )
     )
 
-    cache_paths[path_index] = paths_for_adapter
+    cache_paths[adapter_index] = paths_for_adapter
     return paths_for_adapter
 
 
